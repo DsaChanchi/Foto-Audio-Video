c1944b512cbae64d0945d5c253eca6f70ce4dbd2
DsaChanchi - Troyano <DsaChanchi@users.noreply.github.com>
2021-02-22 10:47:06 +0100

    Update JPGAndMP3ToVideo.py

--- JPGAndMP3ToVideo.py
+++ JPGAndMP3ToVideo.py
@@ -9,13 +9,13 @@ print("Give image:")
 image = input()
 
 # Import the audio
-audio = AudioFileClip(audio+".mp3")
+background = AudioFileClip(audio+".mp3")
 
 # Import the image, create the video and set its duration same as the audio
-video = ImageClip(image+".jpg").set_duration(audio.duration)
+video = ImageClip(image+".jpg").set_duration(background.duration)
 
 # Set the audio of the video
-video = video.set_audio(audio)
+video = video.set_audio(background)
 
 # Export the video
 video.write_videofile(audio+".mp4", fps=24)
