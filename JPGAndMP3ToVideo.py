838a4bf404b741c634c99d4db63cd94591810f4e
DsaChanchi - Troyano <DsaChanchi@users.noreply.github.com>
2021-02-22 01:41:06 +0100

    Add files via upload

---
+++ JPGAndMP3ToVideo.py
@@ -0,0 +1,21 @@
+from moviepy.editor import *
+
+# Keyboard Audio
+print("Give Audio:")
+audio = input()
+
+# Keyboard Image
+print("Give image:")
+image = input()
+
+# Import the audio
+audio = AudioFilevideo(audio+".mp3")
+
+# Import the image, create the video and set its duration same as the audio
+video = Imagevideo(image+".jpg").set_duration(audio.duration)
+
+# Set the audio of the video
+video = video.set_audio(audio)
+
+# Export the video
+video.write_videofile(audio+".mp4", fps=24)
